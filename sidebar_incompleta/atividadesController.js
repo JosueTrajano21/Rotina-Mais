fe8053d9ac3ef991e6041ce2b675740866a8a2e4
Lucas Matheus <57902260+taikipk@users.noreply.github.com>
2025-11-26 18:22:29 -0300

    Add files via upload
    a lógica está nos comentários

---
+++ sidebar_incompleta/atividadesController.js
@@ -0,0 +1,69 @@
+const Atividade = require('../models/atividadesModel')
+    
+
+module.exports = {
+    index: async (req, res) => {
+        const id_paciente = req.session.user.id
+        const filtro = req.query.filter || "Hoje"
+        
+        // Busca as atividades do paciente
+        const tasks = await Atividade.listarPorPaciente(id_paciente)
+
+        // Filtrar tarefas
+        const tarefasFiltradas = "Hoje" // chamar a função para checar a data do model
+
+        const menuItems = [
+            "Sem data", "Hoje", "Semana", "Mês",
+            "Importantes", "Recorrentes", "Vencidas", "Todas"
+        ]
+
+        res.render("pages/atividades", {
+            titulo: "Atividades",
+            css: "atividades.css",
+            tasks: tarefasFiltradas,  //chamaria assim??
+            menuItems,
+            filtro,
+            tarefasFiltradas,
+            selecionado: filtro,
+        })
+    },
+
+    criar: async (req, res) =>{
+
+        const { titulo } = req.body
+        const id_paciente = req.session.user.id // id do paciente
+        console.log(id_paciente)
+        const data = Atividade.formatarDataParaMySQL(new Date()) // teste
+        console.log(data)
+
+        
+
+        if (!titulo || titulo.trim() === '') {
+            const tasks = await Atividade.listarPorPaciente(id_paciente)
+            const menuItems = [
+                "Sem data", "Hoje", "Semana", "Mês",
+                "Importantes", "Recorrentes", "Vencidas", "Todas"
+            ]
+            const selecionado = 'Hoje'
+            const tarefasFiltradas = tasks
+
+            return res.render('pages/atividades', {
+                titulo: "Atividades",
+                css: "atividades.css",
+                tasks: tasks,
+                menuItems: menuItems,
+                selecionado: selecionado,
+                tarefasFiltradas: tarefasFiltradas,
+                error: 'Título é obrigatório!'
+            })
+        }
+
+        await Atividade.criar({
+            id_paciente,
+            titulo,
+            data
+        })
+
+        res.redirect('/atividades')
+    }
+}
---
+++ sidebar_incompleta/atividadesModel.js
@@ -0,0 +1,38 @@
+// criação/listagem de atividades.
+const db = require('../database/db')
+
+class Atividade {
+    
+    static async listarPorPaciente(id_paciente){
+        const [rows] = await db.query(
+            "SELECT * FROM atividade WHERE id_paciente = ?",
+            [id_paciente]
+        )
+        return rows
+    }
+
+    static async criar({id_paciente, titulo, data = null}){
+        console.log("VALOR RECEBIDO EM criar():", { id_paciente, titulo, data })
+        const [resultado] = await db.query(
+            "INSERT INTO atividade (id_paciente, titulo, data) VALUES (?, ?, ?)",
+            [id_paciente, titulo, data]
+        )
+        return resultado.insertId
+    }
+
+
+    static formatarDataParaMySQL(data) {
+        const d = new Date(data)
+
+        const ano = d.getFullYear()
+        const mes = String(d.getMonth() + 1).padStart(2, '0')
+        const dia = String(d.getDate()).padStart(2, '0')
+
+        return `${ano}-${mes}-${dia}`
+    }
+
+    // CRIAR FUNÇÃO PARA CHECAR A DATA PARA O FILTRO
+
+}
+
+module.exports = Atividade;
