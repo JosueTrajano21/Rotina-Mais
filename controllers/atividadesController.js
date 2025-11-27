ff7521093220c05da1927cb15f16fbbe89e1faf8
Lucas Matheus <57902260+taikipk@users.noreply.github.com>
2025-11-26 22:18:47 -0300

    atividadesController.js
    Atualização dos filtros

--- controllers/atividadesController.js
+++ controllers/atividadesController.js
@@ -1,5 +1,26 @@
 const Atividade = require('../models/atividadesModel')
+
+function estaNaSemana(data){
+    if (!data) return false
+    const hoje = new Date()
+    hoje.setHours(0,0,0,0)
     
+    const inicioSemana = new Date(hoje.setDate(hoje.getDate() - hoje.getDay()))
+    console.log(inicioSemana)
+    const fimSemana = new Date(inicioSemana)
+    fimSemana.setDate(inicioSemana.getDate() + 6)
+
+    if (data > inicioSemana && data < fimSemana){
+        return data
+    }
+}
+
+function estaNoMes(data){
+  if (!data) return false
+    const hoje = new Date()
+    return data.getMonth() === hoje.getMonth() &&
+    data.getFullYear() === hoje.getFullYear()  
+}
 
 module.exports = {
     index: async (req, res) => {
@@ -10,7 +31,34 @@ module.exports = {
         const tasks = await Atividade.listarPorPaciente(id_paciente)
 
         // Filtrar tarefas
-        const tarefasFiltradas = 'Hoje'
+        const tarefasFiltradas =  tarefas.filter(tarefa =>{
+            // Filter pq vai retornar os elementos correspondentes a condição
+
+            const hoje = Atividade.formatarDataParaMySQL(new Date())
+
+            if (filtro === "Sem data"){
+                return !(tarefa.data)
+            }
+            if (filtro === "Hoje"){
+                return Atividade.formatarDataParaMySQL(tarefa.data) === hoje
+            } 
+            if (filtro === "Semana"){
+                return  estaNaSemana(tarefa.data)
+            }
+            if (filtro === "Mês"){
+                return  estaNoMes(tarefa.data)
+            }
+            if (filtro === "Importantes"){
+                return tarefa.favorito === 1
+            }
+            if (filtro === "Vencidas"){
+                return Atividade.formatarDataParaMySQL(tarefa.data) < hoje && tarefa.completada === 0;
+            }
+            if (filtro === "Pendentes"){
+                return tarefa.completada === 0;
+            }
+            return true
+        })
 
         const menuItems = [
             "Sem data", "Hoje", "Semana", "Mês",
